8aaaa4b2de0801dbef1de4999bbce659c491a50b
Sam <sam101567@gmail.com>
2023-04-15 20:55:53 +0800

    testing

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ hbs.registerHelper('Equal', function(x, y) {
 
 const app = express();
 app.use(express.static('script')); 
-const port = 80;
+const port = 9090;
 
 // set `hbs` as view engine
 app.set('view engine', 'hbs');
