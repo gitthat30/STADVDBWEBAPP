6752dd2182e4ba52a03205bc95b5c9360505151e
Sam <sam101567@gmail.com>
2023-04-16 15:02:27 +0800

    Implemented Data Replication on Insert

--- index.js
+++ index.js
@@ -10,9 +10,17 @@ var connection = mysql.createConnection({
     database : 'imdb_full'
 });
 
+let node0, node1, node2
+
 connection.connect((error) => {
-    if(error) console.log(error);
-    else console.log("Successful Connection to CCSCLOUD0 - imdb_full")
+    if(error) {
+        console.log(error)
+        node0 = false;
+    }
+    else {
+        node0 = true;
+        console.log("Successful Connection to CCSCLOUD0 - imdb_full")
+    }
 })
 
 var connection2 = mysql.createConnection({
@@ -24,8 +32,14 @@ var connection2 = mysql.createConnection({
 });
 
 connection2.connect((error) => {
-    if(error) console.log(error);
-    else console.log("Successful Connection to CCSCLOUD1 - imdb_b1980")
+    if(error) {
+        console.log(error)
+        node1 = false;
+    }
+    else {
+        node1 = true;
+        console.log("Successful Connection to CCSCLOUD1 - imdb_b1980")
+    }
 })
 
 var connection3 = mysql.createConnection({
@@ -37,8 +51,14 @@ var connection3 = mysql.createConnection({
 });
 
 connection3.connect((error) => {
-    if(error) console.log(error);
-    else console.log("Successful Connection to CCSCLOUD2 - imdb_a1980")
+    if(error) {
+        console.log(error)
+        node2 = false;
+    }
+    else {
+        node2 = true;
+        console.log("Successful Connection to CCSCLOUD2 - imdb_a1980")
+    }
 })
 
 // import module `hbs`
@@ -62,9 +82,22 @@ app.listen(port, function () {
     console.log('app listening at port ' + port);
 });
 
+const test = () => {
+    flag0 = (connection._protocolError == null) && (node0 == true) 
+    flag1 = (connection2._protocolError == null) && (node1 == true)
+    flag2 = (connection3._protocolError == null) && (node2 == true)
+}
+
 // Routes
 
 app.get('/', function(req, res) {
+    
+
+    test()
+
+    console.log("flag 0 = " + flag0)
+    console.log("flag 1 = " + flag1)
+    console.log("flag 2 = " + flag2)
     res.render('first')
 })
 
@@ -84,6 +117,10 @@ app.get('/updatesql', async function(req, res) {
     if(req.query.rank == "") {
         req.query.rank = null
     }
+
+    sql = "UPDATE movies SET name = \'" + req.query.name + "\', year = " + req.query.year + ", `rank` = " + req.query.rank + ", genres = \'" + req.query.genres + "\', director = \'" + req.query.director + "\' where id = " + req.query.id;
+
+
     await new Promise ((resolve) => {
         
         connection.query("UPDATE movies SET name = \'" + req.query.name + "\', year = " + req.query.year + ", `rank` = " + req.query.rank + ", genres = \'" + req.query.genres + "\', director = \'" + req.query.director + "\' where id = " + req.query.id, function (error, results, fields) {
@@ -110,13 +147,16 @@ app.get('/insertsql', async function(req, res) {
     if(req.query.rank == "") {
         req.query.rank = null
     }
-    await new Promise ((resolve) => {
+    
+    sql = "INSERT INTO movies(name, year, `rank`, genres, director) values(\'" + req.query.name + "\', " + req.query.year + ", " + req.query.rank + ", \'" + req.query.genres + "\', \'" + req.query.director + "\')"
+    
+    await new Promise (async (resolve) => {
         
         connection.query("INSERT INTO movies(name, year, `rank`, genres, director) values(\'" + req.query.name + "\', " + req.query.year + ", " + req.query.rank + ", \'" + req.query.genres + "\', \'" + req.query.director + "\')", function (error, results, fields) {
-            if (error) throw error;
+            if (error) resolve(error);
             console.log(results)
             resolve(results)
-          });
+        });
     })
 
     result = await new Promise ((resolve) => {
@@ -126,21 +166,48 @@ app.get('/insertsql', async function(req, res) {
           });
     })
 
+    sql2 = "INSERT INTO movies(id, name, year, `rank`, genres, director) values(" + result[0].id +", \'" + result[0].name + "\', " + result[0].year + ", " + result[0].rank + ", \'" + result[0].genres + "\', \'" + result[0].director + "\')"
+
+    await new Promise (async (resolve) => {
+        if(req.query.year < 1980){
+            connection2.query(sql2, function (error, results, fields) {
+                if (error) resolve(error)
+                else {
+                    console.log("Inserting into node 1")
+                    resolve(results)
+                }
+            });
+        }
+        else {
+            connection3.query(sql2, function (error, results, fields) {
+                if (error) resolve(error)
+                else {
+                    console.log("Inserting into node 2")
+                    resolve(results)
+                }
+            });
+        }
+    });
+
+
     res.render('second', result[0])
 })
 
 app.get('/deletesql', async function(req, res) {
     console.log("DELETE")
+
+    sql = 'DELETE FROM movies WHERE id = ' + req.query.id
+
     result = await new Promise ((resolve) => {
-        connection.query('DELETE FROM movies WHERE id = ' + req.query.id, function (error, results, fields) {
+        connection.query(sql, function (error, results, fields) {
             if (error) throw error;
             resolve(results)
         });
 
         if(req.query.year < 1980){
             console.log("Here")
-            connection2.query('DELETE FROM movies WHERE id = ' + req.query.id, function (error, results, fields) {
-                if (error) throw error;
+            connection2.query(sql, function (error, results, fields) {
+                if (error) resolve(error);
                 else {
                     console.log("Deleting from node 1")
                     resolve(results)
@@ -149,8 +216,8 @@ app.get('/deletesql', async function(req, res) {
         }
 
         else{
-            connection3.query('DELETE FROM movies WHERE id = ' + req.query.id, function (error, results, fields) {
-                if (error) throw error;
+            connection3.query(sql, function (error, results, fields) {
+                if (error) resolve(error);
                 else {
                     console.log("Deleting from node 2")
                     resolve(results)
